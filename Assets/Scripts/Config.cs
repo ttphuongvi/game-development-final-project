dfe66eca300edb6eedbc77823bb9aa5425662632
ttphuongvi <trthphuongvi2901@gmail.com>
2022-05-05 14:03:35 +0700

    limit bird move

---
+++ Assets/Scripts/Config.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Config : MonoBehaviour
+{
+    public bool canMove = false;
+    public float limitMove;
+}
--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -6,31 +6,37 @@ using UnityEngine;
 public class MoveObject : MonoBehaviour
 {
     public GameObject selectedObject;
+    public GameObject cross_bow;
     Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(selectedObject);
+        // Debug.Log(selectedObject);
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
             Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
             if (targetObject)
             {
-                selectedObject = targetObject.transform.gameObject;
+                selectedObject = targetObject.transform.gameObject; // red
                 offset = selectedObject.transform.position - mousePosition;
             }
         }
-
         if (selectedObject)
         {
-            selectedObject.transform.position = mousePosition + offset;
+            Vector3 pos = mousePosition + offset;
+            float limitMove = selectedObject.GetComponent<Config>().limitMove;
+            // float distance = Vector2.Distance(pos, cross_bow.transform.position);
+            float newX = Mathf.Clamp(pos.x, cross_bow.transform.position.x - limitMove, cross_bow.transform.position.x + limitMove);
+            float newY = Mathf.Clamp(pos.y, cross_bow.transform.position.y - limitMove, cross_bow.transform.position.y + limitMove);
+            selectedObject.transform.position = new Vector3(newX, newY, pos.z);
+
         }
 
         if (Input.GetMouseButtonUp(0) && selectedObject)
