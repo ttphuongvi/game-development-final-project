a33fcf00a5d78cdf2ec80190b410789b13e17288
ttphuongvi <trthphuongvi2901@gmail.com>
2022-05-12 16:13:22 +0700

    edit line crossbow

--- Assets/Scripts/CrossBow.cs
+++ Assets/Scripts/CrossBow.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class CrossBow : MonoBehaviour
 {
     public GameObject LeftCrossBow;
-    public GameObject RightCrossbow;
-    public GameObject MainCrossbow;
-    public LineRenderer LeftCrossBowLine;
-    public LineRenderer RightCrossBowLine;
+    public GameObject RightCrossBow;
+    public GameObject MainCrossBow;
+    public LineRenderer Line;
 
     public GameObject SelectedBird;
+
+    public Vector3 middle;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Line.positionCount = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Vector3 realLeft = (LeftCrossBow.transform.position - this.transform.position) / 20 * 100;
+        Vector3 realRight = (RightCrossBow.transform.position - this.transform.position) / 20 * 100;
+        Line.SetPosition(0, realLeft);
+        Line.SetPosition(1, (realLeft + realRight) / 2);
+        Line.SetPosition(2, realRight);
     }
 }
