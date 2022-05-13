b71fbf72df90e2e73cdab6f05342c62f057dcc36
ttphuongvi <trthphuongvi2901@gmail.com>
2022-05-13 14:47:12 +0700

    allow thrown bird

--- Assets/Scripts/CrossBow.cs
+++ Assets/Scripts/CrossBow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,8 @@ public class CrossBow : MonoBehaviour
     public enum CrossBowStateEnum
     {
         Idle,
-        UserPulling
+        UserPulling,
+        BirdFlying
     }
 
 
@@ -15,10 +17,12 @@ public class CrossBow : MonoBehaviour
     public GameObject RightCrossBow;
     public GameObject MainCrossBow;
     public LineRenderer Line;
+    public GameObject TrajectoryLineRenderer;
 
     public GameObject SelectedBird;
-
-    public Vector3 middle, localLeft, localRight, realMidlle, offset;
+    [HideInInspector]
+    public Vector3 middle, localLeft, localRight, realMiddle, offset;
+    public float ThrowSpeed;
 
 
     [HideInInspector]
@@ -33,7 +37,7 @@ public class CrossBow : MonoBehaviour
         localLeft = (LeftCrossBow.transform.position - this.transform.position) / 20 * 100;
         localRight = (RightCrossBow.transform.position - this.transform.position) / 20 * 100;
         middle = (localLeft + localRight) / 2;
-        realMidlle = (LeftCrossBow.transform.position + RightCrossBow.transform.position) / 2;
+        realMiddle = (LeftCrossBow.transform.position + RightCrossBow.transform.position) / 2;
     }
 
     // Update is called once per frame
@@ -44,6 +48,7 @@ public class CrossBow : MonoBehaviour
             case CrossBowStateEnum.Idle:
                 if (Input.GetMouseButtonDown(0))
                 {
+                    // Nhân mút sẽ chuyển sang trạng thái UserPulling
                     Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                     Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
                     if (targetObject && targetObject == SelectedBird.GetComponent<CircleCollider2D>())
@@ -57,19 +62,24 @@ public class CrossBow : MonoBehaviour
             case CrossBowStateEnum.UserPulling:
                 if (Input.GetMouseButton(0))
                 {
+                    // Di chuyển chim theo vị trí chuột
                     Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                     Vector3 pos = mousePosition + offset;
                     Vector2 limitMove = SelectedBird.GetComponent<Bird>().limitMove;
-                    float newX = Mathf.Clamp(pos.x, this.transform.position.x - limitMove.x, this.transform.position.x + limitMove.x);
-                    float newY = Mathf.Clamp(pos.y, this.transform.position.y - limitMove.y, this.transform.position.y + limitMove.y);
+                    float newX = Mathf.Clamp(pos.x, realMiddle.x - limitMove.x, realMiddle.x + limitMove.x);
+                    float newY = Mathf.Clamp(pos.y, realMiddle.y - limitMove.y, realMiddle.y + limitMove.y);
                     SelectedBird.transform.position = new Vector3(newX, newY, pos.z);
                 }
 
                 if (Input.GetMouseButtonUp(0) && SelectedBird)
                 {
-                    SelectedBird.GetComponent<Bird>().Thrown();
-                    // selectedBird = null;
+                    // Ném chuột khi người dùng thả
+                    float distance = Vector3.Distance(realMiddle, SelectedBird.transform.position);
+                    CrossBowState = CrossBowStateEnum.BirdFlying;
+                    ThrowBird(distance);
                 }
+                break;
+            default:
                 break;
         }
 
@@ -84,4 +94,22 @@ public class CrossBow : MonoBehaviour
         }
         Line.SetPosition(2, localRight);
     }
+
+    private void ThrowBird(float distance)
+    {
+        //get velocity
+        Vector3 velocity = realMiddle - SelectedBird.transform.position;
+        SelectedBird.GetComponent<Bird>().Thrown(); //make the bird aware of it
+        //old and alternative way
+        //BirdToThrow.GetComponent<Rigidbody2D>().AddForce
+        //    (new Vector2(v2.x, v2.y) * ThrowSpeed * distance * 300 * Time.deltaTime);
+        //set the velocity
+        SelectedBird.GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x, velocity.y) * ThrowSpeed * distance;
+
+
+        //notify interested parties that the bird was thrown
+        if (BirdThrown != null)
+            BirdThrown(this, EventArgs.Empty);
+    }
+    public event EventHandler BirdThrown;
 }
