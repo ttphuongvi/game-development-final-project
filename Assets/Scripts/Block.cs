081851224d8a5150a5ea28aff8c8754fa3989998
Phong <phong940253@gmail.com>
2022-05-15 17:34:55 +0700

    change sprite with heath for block

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -7,13 +7,41 @@ public class Block : MonoBehaviour
     public float maxHeath;
     [HideInInspector]
     public float health;
+    public Sprite[] spriteArray;
+
+    public void changeSprite(int index)
+    {
+        GetComponent<SpriteRenderer>().sprite = spriteArray[index];
+    }
 
     void OnCollisionEnter2D(Collision2D col) {
+
         if (col.gameObject.GetComponent<Rigidbody2D>() == null) return;
+
+        // Tính damage, detroy nếu hết máu
         float damage = col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10;
         health -= damage;
         if (health <= 0) Destroy(this.gameObject);
         Debug.Log(damage);
+
+        // Check máu chuyển đổi Sprite
+        if (health >= maxHeath * 0.75f)
+        {
+            changeSprite(0);
+        }
+        else if (health >= maxHeath * 0.5f)
+        {
+            changeSprite(1);
+        }
+        else if (health >= maxHeath * 0.25f)
+        {
+            changeSprite(2);
+        }
+        else
+        {
+            changeSprite(3);
+        }
+
     }
 
     // Start is called before the first frame update
