96dcc4b9156571ea5db7ace8bc5a81c8f5536f01
Phong <phong940253@gmail.com>
2022-05-17 15:07:08 +0700

    add UI ingame

---
+++ Assets/Scripts/IngameMenu.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class IngameMenu : MonoBehaviour
+{
+    public VisualElement root;
+
+    public Button btnPause, btnReturn;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        root = GetComponent<UIDocument>().rootVisualElement;
+        btnPause = root.Q<Button>("btnPause");
+        btnPause.clicked += OnPauseClicked;
+
+        btnReturn = root.Q<Button>("btnReturn");
+        btnReturn += OnReturnClicked;
+    }
+
+    void OnPauseClicked()
+    {
+        Debug.Log("Clicked");
+    }
+
+    void OnReturnClicked()
+    {
+        Debug.Log("Clicked");
+    }
+
+}
