82d74cb998f07842e4b38cf37d6164b0e2111247
Phong <phong940253@gmail.com>
2022-05-17 19:52:52 +0700

    fix UI pause

--- Assets/Scripts/IngameMenu.cs
+++ Assets/Scripts/IngameMenu.cs
@@ -3,27 +3,42 @@ using UnityEngine.UIElements;
 
 public class IngameMenu : MonoBehaviour
 {
-    public VisualElement root;
-
-    public Button btnPause, btnReturn;
+    [HideInInspector]
+    public VisualElement root, playState, pauseState, gameOverState;
+    [HideInInspector]
+    public Button btnPause, btnReturn, btnResume;
     // Start is called before the first frame update
     void OnEnable()
     {
         root = GetComponent<UIDocument>().rootVisualElement;
         btnPause = root.Q<Button>("btnPause");
-        // btnPause.clicked += OnPauseClicked;
+        btnPause.clicked += OnPauseClicked;
 
         btnReturn = root.Q<Button>("btnReturn");
-        // btnReturn += OnReturnClicked;
+        btnReturn.clicked += OnReturnClicked;
+
+        btnResume = root.Q<Button>("btnResume");
+        btnResume.clicked += OnResumeClicked;
+
+        playState = root.Q<VisualElement>("playState");
+        pauseState = root.Q<VisualElement>("pauseState");
     }
 
     void OnPauseClicked()
     {
-        Debug.Log("Clicked");
+        playState.style.display = DisplayStyle.None;
+        pauseState.style.display = DisplayStyle.Flex;
+        // Debug.Log("Pause");
     }
 
     void OnReturnClicked()
     {
+        Debug.Log("Clicked");
+    }
+
+    void OnResumeClicked() {
+        playState.style.display = DisplayStyle.Flex;
+        pauseState.style.display = DisplayStyle.None;
         Debug.Log("Clicked");
     }
 
