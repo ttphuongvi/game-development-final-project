a2d55a5785dbb42fa56e2970bedc90d46c8cd46b
Phong <phong940253@gmail.com>
2022-05-18 20:40:02 +0700

    fix collider for pig

--- Assets/Scripts/Pig.cs
+++ Assets/Scripts/Pig.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class Pig : MonoBehaviour
 {
-    public float defend;
+    public float maxHeath = 100;
+    
+    [ShowOnly] public float health;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    [HideInInspector]
+    public Animator animator;
+    public enum PigState
     {
-        if (collision.relativeVelocity.magnitude > defend)
-        {
-            Destroy(gameObject, 0.1f);
+        Idle,
+        Injured,
+        SeriousInjured,
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent<Rigidbody2D>() == null) return;
+
+        float damage = col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 100;
+        health -= damage;
+        if (health <= 0) {
+            // animator.SetInteger("State", 2);
+            GetComponent<SpriteRenderer>().GetComponent<Renderer>().enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+            Destroy(this.gameObject, 2f);
+        }
+
+        if (health <= maxHeath * 0.3f) {
+            animator.SetInteger("State", 2);
+        }
+        else if (health <= maxHeath * 0.6f) {
+            animator.SetInteger("State", 1);
+        } else {
+            animator.SetInteger("State", 0);
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        health = maxHeath;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
